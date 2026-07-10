b01692a6a231fe53cfe98f49e6f03f7c75a8bcb4
Yuna <yuna.bonnifet@gmail.com>
2026-07-10 15:05:25 +0200

    Add Wind

---
+++ Assets/Scripts/Obstacles/Wind.cs
@@ -0,0 +1,30 @@
+using Mono.Cecil.Cil;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    public enum WindDirection2D { Up, Down, Left, Right }
+    
+    [SerializeField] private WindDirection2D chooseDirection = WindDirection2D.Right;
+    [SerializeField] private float windStrength = 10f;
+
+    private void OnTriggerStay2D(Collider2D collision) 
+    {
+        if (collision.gameObject.TryGetComponent<BeeController>(out var bee))
+            bee.rb.AddForce(GetWindDirection() * windStrength, ForceMode2D.Force);
+    }
+
+    private Vector2 GetWindDirection()
+    {
+        var direction = chooseDirection switch
+        {
+            WindDirection2D.Up => transform.up,
+            WindDirection2D.Down => -transform.up,
+            WindDirection2D.Left => -transform.right,
+            WindDirection2D.Right => transform.right,
+            _ => transform.up
+        };
+
+        return direction;
+    }
+}
