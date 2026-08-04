554279f53cfa37afc9d905775165be6e978ad3b3
Yuna <yuna.bonnifet@gmail.com>
2026-08-04 15:31:01 +0200

    Opti project for build

--- Assets/Scripts/Optimizer.cs
+++ Assets/Scripts/Optimizer.cs
@@ -1,14 +1,42 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Optimizer : MonoBehaviour
 {
+    private int temporaryLayer; // Caching the layer ID for perf instead of a string every frame
+
+    private void Start()
+    {
+        temporaryLayer = LayerMask.NameToLayer("Temporary");
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Player entered the optimization zone.");
+        if (other.gameObject.layer == temporaryLayer)
+        {
+            Light2D lightComponent = other.GetComponent<Light2D>();
+            
+            if (lightComponent != null)
+                lightComponent.enabled = true; // Turn the light ON
+
+            else
+            {
+                if (other.transform.childCount > 0)
+                    other.transform.GetChild(0).gameObject.SetActive(true); // Activate particles   
+            }     
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log("Player exited the optimization zone.");
+        if (other.gameObject.layer == temporaryLayer)
+        {
+            Light2D lightComponent = other.GetComponent<Light2D>();
+            if (lightComponent != null)
+                lightComponent.enabled = false; // Turn the light OFF
+            
+            else
+                other.gameObject.SetActive(false); // Deactivate particles
+        }        
     }
 }
